5d7b9635b880ebc8b682e4bf97b77d1cb628efcb
Mylene Leblanc <leblanc.pommier.mylene@gmail.com>
2024-02-27 15:48:59 +0100

    first style

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -2,4 +2,13 @@ class CostumesController < ApplicationController
   def index
     @costumes = Costume.all
   end
+
+  # def new
+  #   @costume = Costume.new
+  # end
+
+  # def create
+  #   @costume = Costume.new
+  #   @costume
+  # end
 end
