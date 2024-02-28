9f6d05bee7a992e9af551c98e02fc872b9e9c5cb
Mylene Leblanc <leblanc.pommier.mylene@gmail.com>
2024-02-28 13:54:15 +0100

    search_form_navbar

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,4 +1,9 @@
 class Costume < ApplicationRecord
+  def self.types
+    ["mammifère", "lombric", "rapace", "reptile", "insecte"]
+  end
+
   belongs_to :user
   validates :name, presence: true, length: { minimum: 6 }
+  validates :costume_type, inclusion: Costume.types
 end
