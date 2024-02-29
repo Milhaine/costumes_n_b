925ae5473ddf79736d6f2f556b5707f378a545ef
Romain Potier Ferry <romainpotierferry@gmail.com>
2024-02-29 15:24:00 +0100

    implemented review action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,10 +4,22 @@ class ReviewsController < ApplicationController
   end
 
   def new
+    @costume = Costume.find(params[:costume_id])
     @review = Review.new
   end
 
   def create
     @user = current_user
+    @review = Review.new(review_params)
+    @review.user = @user
+    @review.costume = Costume.find(params[:costume_id])
+    @review.save
+    redirect_to costume_path(@review.costume)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
   end
 end
